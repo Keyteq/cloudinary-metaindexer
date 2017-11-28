41b755d8f981ddaa1085fdd411df2635d6ada996
Petter Kjelkenes <kjelkenes@gmail.com>
2017-11-28 09:06:25 +0100

    Fixes dynamic controller injection

--- DependencyInjection/Compiler/CompilerPass.php
+++ DependencyInjection/Compiler/CompilerPass.php
@@ -8,12 +8,6 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 class CompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
-    {        // For 6.X+ we use the new ContentView.
-        if (class_exists('\eZ\Publish\Core\MVC\Symfony\View\BaseView')) {
-            $container->setParameter('keyteq_cloudinary_meta_indexer.controller.full', 'keyteq.cloudinary_meta_indexer.controller.full_view:viewCloudinaryPage');
-        } else {
-            // Fallback to use old controller.
-            $container->setParameter('keyteq_cloudinary_meta_indexer.controller.full', 'keyteq.cloudinary_meta_indexer.controller.full_view:viewCloudinaryPageLocation');
-        }
+    {
     }
 }
--- DependencyInjection/KeyteqCloudinaryMetaIndexerExtension.php
+++ DependencyInjection/KeyteqCloudinaryMetaIndexerExtension.php
@@ -4,12 +4,13 @@ namespace Keyteq\Bundle\CloudinaryMetaIndexer\DependencyInjection;
 
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Yaml\Yaml;
 
-class KeyteqCloudinaryMetaIndexerExtension extends Extension
+class KeyteqCloudinaryMetaIndexerExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -29,7 +30,15 @@ class KeyteqCloudinaryMetaIndexerExtension extends Extension
     {
         $configFile = __DIR__ . '/../Resources/config/ezplatform.yml';
         $config = Yaml::parse(file_get_contents($configFile));
-        $container->prependExtensionConfig('ezpublish', $config);
+
+        // For 6.X+ we use the new ContentView.
+        if (class_exists('\eZ\Publish\Core\MVC\Symfony\View\BaseView')) {
+            $config['ezpublish']['system']['default']['content_view']['full']['cloudinary_page']['controller'] = 'keyteq.cloudinary_meta_indexer.controller.full_view:viewCloudinaryPage';
+        } else {
+            $config['ezpublish']['system']['default']['content_view']['full']['cloudinary_page']['controller'] = 'keyteq.cloudinary_meta_indexer.controller.full_view:viewCloudinaryPageLocation';
+        }
+
+        $container->prependExtensionConfig('ezpublish', $config['ezpublish']);
         $container->addResource(new FileResource($configFile));
     }
 }
